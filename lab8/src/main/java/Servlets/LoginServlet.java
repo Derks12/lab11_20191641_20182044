4ca3cf289f81ae75ad3a287d24b9a1757abb08ad
Diego Henriquez <a20182044@pucp.edu.pe>
2023-11-13 20:08:10 -0500

    añadiendo cambios

--- lab8/src/main/java/Servlets/LoginServlet.java
+++ lab8/src/main/java/Servlets/LoginServlet.java
@@ -1,5 +1,6 @@
 package Servlets;
 
+import Beans.trabajadores;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -11,12 +12,25 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession httpSession = request.getSession();
-        Logged = (Employee) httpSession.getAttribute("usuarioLogueado");
+        trabajadores trabajadoresLogged = (trabajadores) httpSession.getAttribute("usuarioLogueado");
+
+        if(trabajadoresLogged != null && trabajadoresLogged.getDni() != null ){
+            if(request.getParameter("a") != null){
+                httpSession.invalidate();;
+            }
+            response.sendRedirect(request.getContextPath());
+        }else{
+            request.getRequestDispatcher("loginForm.jsp").forward(request,response);
+        }
 
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String usarname = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        System.out.println("username: " +usarname + "| password: " + password);
 
     }
 }
