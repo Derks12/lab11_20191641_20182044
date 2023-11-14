be284a5c1bfef3717c8a80017606fecab417eff6
Alex <a20191641@pucp.edu.pe>
2023-11-13 19:39:07 -0500

    beans

---
+++ lab8/src/main/java/Beans/trabajadores.java
@@ -0,0 +1,51 @@
+package Beans;
+
+public class trabajadores {
+
+    private String nombres;
+    private String apellidos;
+    private String correo;
+    private String dni;
+    private Integer idsede;
+
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public Integer getIdsede() {
+        return idsede;
+    }
+
+    public void setIdsede(Integer idsede) {
+        this.idsede = idsede;
+    }
+}
