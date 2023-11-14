fa0891f153c080b09709ed9eb0e5d18bde8dd0ce
Alex <a20191641@pucp.edu.pe>
2023-11-13 19:54:39 -0500

    asda

--- lab8/src/main/java/Daos/trabajadorDao.java
+++ lab8/src/main/java/Daos/trabajadorDao.java
@@ -1,4 +1,56 @@
 package Daos;
 
-public class trabajadorDao {
+import java.sql.*;
+import Beans.trabajadores;
+import Daos.DaoBase;
+
+public class trabajadorDao extends DaoBase{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public boolean validarUsuarioPasswordHashed(String username, String password){
+
+        String sql = "SELECT * FROM employees_credentials where email = ? and password_hashed = sha2(?,256)";
+
+        boolean exito = false;
+
+        try(Connection connection = getConection();
+            PreparedStatement pstmt = connection.prepareStatement(sql)){
+
+            pstmt.setString(1,username);
+            pstmt.setString(2,password);
+
+            try(ResultSet rs = pstmt.executeQuery()){
+                if(rs.next()){
+                    exito = true;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return exito;
+    }
+
+
+
+
+
+
+
+
+
 }
